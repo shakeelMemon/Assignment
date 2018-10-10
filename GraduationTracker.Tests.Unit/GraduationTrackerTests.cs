125b6164c7a8103b682d6f80c3c5d7062088d2b6
Shakeel.Memon <shakeelshp2020@hotmail.com>
2018-10-09 19:31:32 -0600

    added

---
+++ GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraduationTracker.Tests.Unit
+{
+    [TestClass]
+    public class GraduationTrackerTests
+    {
+        [TestMethod]
+        public void TestHasCredits()
+        {
+            var tracker = new GraduationTracker();
+
+            var diploma = TestUtility.GenerateDiploma();
+            var students = TestUtility.GenerateStudents(4);                
+            var graduated = new List<Tuple<bool, STANDING>>();
+            foreach(var student in students)            
+                graduated.Add(tracker.HasGraduated(diploma, student));    
+
+            Assert.IsTrue(graduated.Any());
+            Assert.IsTrue(graduated.Count == 4);
+            Assert.IsTrue(graduated.Where(x => x.Item2 == STANDING.Average).Count() == 2);
+            Assert.IsTrue(graduated.Where(x => x.Item2 == STANDING.Remedial).Count() == 2);
+            Assert.IsTrue(graduated.Where(x => x.Item1 == true).Count() == 2);
+            Assert.IsTrue(graduated.Where(x => x.Item1 == false).Count() == 2);
+        }
+
+
+
+    }
+}
---
+++ GraduationTracker.Tests.Unit/TestUtility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker.Tests.Unit
+{
+    public class TestUtility
+    {
+
+        static string[] courses = { "Math", "Science", "Literature", "Physichal Education" };
+        static int[] marks = { 95, 80, 50, 40 };
+
+
+        public static Diploma GenerateDiploma()
+        {
+            return new Diploma
+            {
+                Id = 1,
+                Credits = 4,
+                Requirements = new int[] { 100, 102, 103, 104 }
+            };
+        }
+        public static Student[] GenerateStudents(int studentCount)
+        {
+            Student[] students = new Student[studentCount];
+
+            if (studentCount > 4)
+                return students;
+
+            for (int i = 0; i < studentCount; i++)            
+                students[i] = GenerateStudent(0, marks[i]);         
+
+            return students;
+        }
+
+        private static Student GenerateStudent(int id, int marks)
+        {
+            Student student = new Student() { Id = id };
+            int courseCount = 4;
+
+            student.Courses = new Course[courseCount];            
+            for(int i=0; i<courseCount; i++)
+                student.Courses[i] = GenerateCourse(i, courses[i], marks);
+
+            return student;
+        }
+        
+        private static Course GenerateCourse(int id, string name, int mark)
+        {
+            return new Course { Id = id, Name = name, Mark = mark };
+        }
+    }
+}
